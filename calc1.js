ea13d99b5a35d60a745f1a70c54ba94d4fb29426
Kelsea W <kelsea.wessel@google.com>
2026-05-21 11:50:57 -0500

    Functions created!

--- calc1.js
+++ calc1.js
@@ -10,8 +10,8 @@ function absoluteValue(num){
 
 //Power Caluclation -  Calculate and return the value of a base raised to a specific power.
 
-function powerCalculation(num){
-    return Math.
+function powerCalculation(base, exponent){
+    return Math.pow(base, exponent);
 }
 
 //Square Root Calculation - Calculate the square root of a number.
@@ -22,11 +22,46 @@ function rootCalculation(num){
 
 //Maximum and Mimimun Finder - From a given set of numbers, determine the largest and smallest values.
 
+function maxAndMin(num1, num2){
+    console.log(`The Max Value is: ${Math.max(num1, num2)}`);
+    console.log(`The Min Value is: ${Math.min(num1, num2)}`);
+    return(0);
+}
+
 //Random Number Generator: Generate a random integer within a specified range.
 function randomNumber(max, min){
-    return Math.random()*(max - min) + min;
+    let randomNum = Math.random()*(max - min) + min;
+    return randomNum.toFixed(0);
 }
 
 //Custom Rounding: Round a number to a specified number of decimal places.
 
+function customRounding(num, places){
+    let roundedNumber = Math.round(num * 10000)/10000;
+    return roundedNumber;
+}
+
+console.log(absoluteValue(42));
+console.log(absoluteValue(-42));
+
+console.log(powerCalculation(2,2))
+console.log(powerCalculation(10,10))
+
+console.log(rootCalculation(500));
+
+maxAndMin(25, 400);
+
+
 console.log(randomNumber(300, 1));
+
+console.log(customRounding(5.60293802384092834098234));
+
+
+/// User needs to be able to
+// Select operations
+// input required values
+// Calculator needs to provide options to select, prompt input, and provide results
+
+//use if statements to prompt user like a phone menu to select the desired function
+//then prompt the user for inputs before calling the desired function
+//once provided, the function is called OR returns an error that input was not valid for function
